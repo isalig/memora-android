72a95b375e42699eb1a5b47760ab5a3836552c73
Alexander Ischuk <a.ischuk@redmadrobot.com>
2020-03-23 00:00:08 +0300

    Initialize project.

---
+++ app/src/main/java/io/aiico/memora/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.aiico.memora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
